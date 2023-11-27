ca0c78c4e4545ae04d44aa0cffc4b978177eef73
Alexander Metelkov <metel-sanya@mail.ru>
2023-11-27 22:52:09 +0300

    output of films is made, film check and deletion

--- js/index.js
+++ js/index.js
@@ -4,24 +4,27 @@ const moviesOutputNode = document.getElementById('movies');
 const validationMessageNode = document.getElementById('validationMessage');
 
 const TITLE_VALIDATION_MAX = 10;
+const STORAGE_LABEL_MOVIES = 'movies';
 
 const minTitleLimitMessage = 'Фильм без названия не найти';
 const maxTitleLimitMessage = 'Очень длинное название';
 
 let movies = [];
 
-// const saveMoviesToLocalStorage = () => {
-// 	localStorage.setItem('movies', JSON.stringify(movies));
-// }
+const saveMoviesStorage = (key, value) => {
+	return localStorage.setItem(key, JSON.stringify(value));
+}
 
-function validation(movieFromUser) {
+const getMoviesFromStorage = (key) => {
+	return JSON.parse(localStorage.getItem(key));
+}
 
+const validation = (movieFromUser) => {
 	let result = true;
 
 	if (movieFromUser === '') {
 		validationMessageNode.className = 'validationMessage';
 		validationMessageNode.innerText = minTitleLimitMessage;
-		movieAddBtnNode.disabled = true;
 		result = false;
 		return result;
 	}
@@ -29,46 +32,49 @@ function validation(movieFromUser) {
 	if (movieFromUser > TITLE_VALIDATION_MAX) {
 		validationMessageNode.className = 'validationMessage';
 		validationMessageNode.innerText = maxTitleLimitMessage;
-		movieAddBtnNode.disabled = true;
 		result = false;
 		return result;
 	} 
 
 		validationMessageNode.className = 'validationMessage__hidden';
-		movieAddBtnNode.disabled = false;
 
 		return result;
 };
 
-function newPostMovieHandler() {
+// получение названия фильма из инпута
+const getMovieFromUser = () => {
+	const movieName = titleMovieInputNode.value.trim();
+
+	clearInput();
+	
+	return movieName;
+}
+
+const newPostMovieHandler = () => {
 	const movieFromUser = getMovieFromUser();
 
 	if (!validation(movieFromUser)) {
 		return;
 	}
 
-	movies.push(movieFromUser);
+	const movie = {
+		name: movieFromUser,
+		checkbox: 'unchecked',
+	}
 
+	movies.push(movie);
+	
+	saveMoviesStorage(STORAGE_LABEL_MOVIES, movies);
 	renderMovies(movies);
 }
 
-// получение названия фильма из инпута
-function getMovieFromUser() {
-		const movieName = titleMovieInputNode.value.trim();
-
-		clearInput();
-
-		return movieName;
-}
-
 //очистка инпута
-function clearInput() {
-	titleMovieInputNode.value = "";
+const clearInput = () =>{
+	titleMovieInputNode.value = '';
 };
 
-
 // отображение фильма
-function renderMovies(movies) {
+const renderMovies = (movies) => {
 	moviesOutputNode.innerHTML = '';
 
 	const movieWrapper = document.createElement('ul');
@@ -85,13 +91,15 @@ function renderMovies(movies) {
 		const wrapperCheckbox = document.createElement('input');
 		wrapperCheckbox.className = 'real__checkbox';
 		wrapperCheckbox.setAttribute('type', 'checkbox');
+		wrapperCheckbox.setAttribute(movie.checkbox, '');
 
 		const wrapperCheckboxClick = document.createElement('div');
 		wrapperCheckboxClick.className = 'checkbox__click';
 
 		const wrapperMovieName = document.createElement('span');
 		wrapperMovieName.className = 'movie__name';
-		wrapperMovieName.innerText = movie;
+
+		wrapperMovieName.innerText = movie.name;
 
 		const wrapperMovieCloseBtn = document.createElement('button');
 		wrapperMovieCloseBtn.className = 'movie__close__btn';
@@ -107,30 +115,48 @@ function renderMovies(movies) {
 		movieWrapper.appendChild(wrapperMovieItem);
 
 		wrapperCheckbox.addEventListener('click', () => {
-			if (movie.check === 'unchecked') {
-				movie.check = 'checked';
+			if (movie.checkbox === 'unchecked') {
+				console.log(movie.checkbox)
+				movie.checkbox = 'checked';
 			} else {
-				movie.check;
+				movie.checkbox = 'unchecked';
 			}
+			saveMoviesStorage(STORAGE_LABEL_MOVIES, movies);
 		});
 
 		wrapperMovieCloseBtn.addEventListener('click', function() {
 			// удаление одного элемента из массива
 			movies.splice(index, 1);
-			renderMovies(movies)
-			});
 
+			saveMoviesStorage(STORAGE_LABEL_MOVIES, movies);
+			renderMovies(movies);
+			});
 });
 
 moviesOutputNode.appendChild(movieWrapper)
 };
 
-function clickToEnter(e) {
+const clickToEnter = (e) => {
 	if(e.keyCode === 13) {
 		e.preventDefault();
 		newPostMovieHandler();
 	}
 }
+
+const init = () => {
+
+	let moviesArray = getMoviesFromStorage(STORAGE_LABEL_MOVIES);
+
+	if (!moviesArray == []) {
+		movies = moviesArray;
+	} else {
+		movies = [];
+	}
+
+	renderMovies(movies);
+}
+
+init();
 
 titleMovieInputNode.addEventListener('keydown', clickToEnter);
 titleMovieInputNode.addEventListener('input', validation);
